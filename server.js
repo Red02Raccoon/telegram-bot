e28b32d9cbca8d9ce5e5408bb1dde8d87604d569
Chervona <nastya02chervona@gmail.com>
2019-01-31 17:05:11 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const { token } = require('./config');
+var TelegramBot = require('node-telegram-bot-api');
+
+var bot = new TelegramBot(token, {polling: true}); // Включить опрос сервера
+
+bot.on('message', (msg) => {
+
+  var hi = "hi";
+  if (msg.text.toString().toLowerCase().indexOf(hi) === 0) {
+    bot.sendMessage(msg.chat.id,"Hello," + msg.from.first_name);
+  } 
+      
+  var bye = "bye";
+  if (msg.text.toString().toLowerCase().includes(bye)) {
+    bot.sendMessage(msg.chat.id, "Hope to see you around again , Bye");
+  }
+
+  var robot = "I'm robot";
+  if (msg.text.indexOf(robot) === 0) {
+      bot.sendMessage(msg.chat.id, "Yes I'm robot but not in that way!");
+  }
+
+});
+
+bot.onText(/\/start/, (msg) => {
+  bot.sendMessage(msg.chat.id, "Welcome");
+});
+
+bot.onText(/\/sendpic/, (msg) => {
+  bot.sendPhoto(msg.chat.id,"https://marketplace.canva.com/MADGwM67DJw/5/thumbnail_large/canva-pink-petaled-flowers-closeup-photo-MADGwM67DJw.jpg", {caption : "Here we go ! \nThis is just a caption "} );  
+});
